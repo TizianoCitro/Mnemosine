437961e9ecd50c074d868670ce12cae20a48dff7
Tiziano <tizianocitro98@gmail.com>
2020-12-15 16:22:51 +0100

    Initial commit

---
+++ Mnemosine/src/main/java/mnemosine/mnemosine/MnemosineApplication.java
@@ -0,0 +1,11 @@
+package mnemosine.mnemosine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MnemosineApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MnemosineApplication.class, args);
+	}
+}
