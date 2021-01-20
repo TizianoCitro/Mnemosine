8d4cd2181c346fbca4adb2a3362a4664ff8d4a1a
Tiziano <tizianocitro98@gmail.com>
2021-01-20 13:13:59 +0100

    Logging

--- Mnemosine/src/main/java/mnemosine/controller/StorageAccountController.java
+++ Mnemosine/src/main/java/mnemosine/controller/StorageAccountController.java
@@ -19,17 +19,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.logging.Logger;
+
 @RestController
 @RequestMapping(value = "/account")
 public class StorageAccountController {
 
+    Logger logger = Logger.getLogger(StorageAccountController.class.getName());
+
     @PostMapping("/create")
     public MnemosineDTO<StorageAccountCreateDTO> create(@RequestBody StorageAccountCreateRequestDTO requestDTO) {
+        logger.info("Sono nel servizio 'account/create'"
+                + " per creare l'account: " + requestDTO.getAccountName()
+                + " nel gruppo: " + requestDTO.getGroupName());
+
         return storageAccountService.create(requestDTO);
     }
 
     @DeleteMapping("/delete")
     public MnemosineDTO<StorageAccountDeleteDTO> delete(@RequestBody StorageAccountDeleteRequestDTO requestDTO) {
+        logger.info("Sono nel servizio 'account/delete'"
+                + " per eliminare l'account: " + requestDTO.getAccountName()
+                + " nel gruppo: " + requestDTO.getGroupName());
+
         return storageAccountService.delete(requestDTO);
     }
 
@@ -50,6 +62,8 @@ public class StorageAccountController {
             @RequestParam("secret") String secret,
             @RequestParam("subscription_id") String subscriptionId,
             @RequestParam("group_name") String groupName) {
+        logger.info("Sono nel servizio 'account/accounts-by-group' per gli accopunt nel gruppo: " + groupName);
+
         return storageAccountService.listAccountsByResourceGroup(
                 new StorageAccountListRequestDTO(clientId, tenantId, secret, subscriptionId, groupName));
     }
@@ -62,6 +76,10 @@ public class StorageAccountController {
             @RequestParam("subscription_id") String subscriptionId,
             @RequestParam("group_name") String groupName,
             @RequestParam("account_name") String accountName) {
+        logger.info("Sono nel servizio 'account/info'"
+                + " per info sull'accopunt: " + accountName
+                + " nel gruppo: " + groupName);
+
         return storageAccountService.accountInfo(
                 new StorageAccountInfoRequestDTO(clientId, tenantId, secret, subscriptionId, groupName, accountName));
     }
