93a79c2f4452b049042eb8912b28b09b21727db7
Tiziano <tizianocitro98@gmail.com>
2021-01-28 11:54:11 +0100

    War packaging for deployment

--- Mnemosine/src/main/java/mnemosine/mnemosine/MnemosineApplication.java
+++ Mnemosine/src/main/java/mnemosine/mnemosine/MnemosineApplication.java
@@ -2,12 +2,19 @@ package mnemosine.mnemosine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan({"mnemosine.controller", "mnemosine.service", "mnemosine.config", "mnemosine.swagger"})
 @SpringBootApplication
-public class MnemosineApplication {
+public class MnemosineApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(MnemosineApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MnemosineApplication.class);
 	}
 }
